d7fdc5f2fdb2cc7226d8fd77afac8ea910946cb3
Anna, Siva (Contractor) <Siva_Anna2@comcast.com>
2018-03-09 07:04:18 -0700

    modified script

--- Restful_Return_Xml_Json/script/script.sh
+++ Restful_Return_Xml_Json/script/script.sh
@@ -8,6 +8,13 @@ MANIFEST_FILE=${6}
 APP_FILE=${7}
 
 echo "Before Deployment Start"
+echo ${TARGET_URL}
+echo ${CF_USER}
+echo ${CF_PWD}
+echo ${CF_ORG}
+echo ${CF_SPACE}
+echo ${MANIFEST_FILE} 
+echo ${CF_SPACE}
 cf login ${TARGET_URL} -u ${CF_USER} -p ${CF_PWD} -o ${CF_ORG} -s ${CF_SPACE}
 echo "Logged into Cloud Foundry"
 cf push -f ${MANIFEST_FILE} -p ${APP_FILE}
