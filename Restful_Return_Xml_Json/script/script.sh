b348ade96671afdc28bb022ae62232cff4ce3123
Anna, Siva (Contractor) <Siva_Anna2@comcast.com>
2018-03-09 06:37:54 -0700

    modified script

--- Restful_Return_Xml_Json/script/script.sh
+++ Restful_Return_Xml_Json/script/script.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#TARGET_URL=https://api.run.pivotal.io
-#CF_USER=a.s.j.nivetha14@gmail.com
-#CF_PWD=SwamiJo@05
-#CF_ORG=SampleSpace
-#CF_SPACE=development
-#MANIFEST_FILE=src/main/resources/manifest.yml
-#APP_FILE=target/Restful_Return_Xml_Json.war
+TARGET_URL=${1}
+CF_USER=${2}
+CF_PWD=${3}
+CF_ORG=${4}
+CF_SPACE=${5}
+MANIFEST_FILE=${6}
+APP_FILE=${7}
 
 echo "Before Deployment Start"
 cf login ${TARGET_URL} -u ${CF_USER} -p ${CF_PWD} -o ${CF_ORG} -s ${CF_SPACE}
