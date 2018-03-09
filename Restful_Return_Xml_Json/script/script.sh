4f26593572431e983aaff6ea2e6fbfd27ca53858
Anna, Siva (Contractor) <Siva_Anna2@comcast.com>
2018-03-09 07:35:43 -0700

    modified script

--- Restful_Return_Xml_Json/script/script.sh
+++ Restful_Return_Xml_Json/script/script.sh
@@ -15,6 +15,7 @@ echo ${CF_ORG}
 echo ${CF_SPACE}
 echo ${MANIFEST_FILE} 
 echo ${APP_FILE}
+echo "Found Env variables"
 cf login -a ${TARGET_URL} -u ${CF_USER} -p ${CF_PWD} -o ${CF_ORG} -s ${CF_SPACE}
 echo "Logged into Cloud Foundry"
 cf push -f ${MANIFEST_FILE} -p ${APP_FILE}
